11d7ba0959b5850e822151f334ab6d13853284ef
Pasquale Iero <pasquale.iero@open.ac.uk>
2019-10-29 14:38:51 +0000

    Attempt submitted

--- src/transformer/get_event_function_map.php
+++ src/transformer/get_event_function_map.php
@@ -73,6 +73,7 @@ function get_event_function_map() {
         '\mod_survey\event\response_submitted' => 'mod_survey\response_submitted',
         '\mod_questionnaire\event\course_module_viewed' => 'mod_questionnaire\course_module_viewed',
         '\mod_questionnaire\event\attempt_saved' => 'mod_questionnaire\attempt_saved',
+        '\mod_questionnaire\event\attempt_submitted' => 'mod_questionnaire\attempt_submitted',
         '\mod_url\event\course_module_viewed' => 'mod_url\course_module_viewed',
         '\mod_wiki\event\course_module_viewed' => 'all\course_module_viewed',
         '\mod_workshop\event\course_module_viewed' => 'all\course_module_viewed',
